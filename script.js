6b00abe0c3c5ea319776219b66c7d89ec037f167
Ezra <97122016+ezra-casas@users.noreply.github.com>
2024-03-16 03:08:18 -0700

    blocks functioning

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const grid = document.querySelector(".grid");
 
-let gridSize = 20; 
+let gridSize = 20;
 
-for (let i = 0; i < gridSize; i++){
+for (let i = 0; i < gridSize; i++) {
   let parentDiv = document.createElement("div")
   parentDiv.classList.add(`row`)
-//  parentDiv.classList.add(`${i+1}`)
 
   for (let j = 0; j < gridSize; j++) {
     let div = document.createElement("div")
 
     div.classList.add("block")
-    div.textContent = `${j+1}`
+    div.setAttribute("id", `row:${i+1}-col:${j+1}`)
+    
+    div.addEventListener("mouseenter",  e => {
+      div.style.backgroundColor = "#fff"
+    })
 
-    parentDiv.appendChild(div)
-  }
+    parentDiv.appendChild(div);
+  };
   grid.appendChild(parentDiv)
 }
 
 
-// console.log(grid)
