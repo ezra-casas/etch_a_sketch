3cf0473d73699ac56572d9e28bb68d47a8cad7a4
Ezra <97122016+ezra-casas@users.noreply.github.com>
2024-03-18 02:55:06 -0700

    grid buttons functional

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
 const grid = document.querySelector(".grid");
+let gridSize = 16; 
 
-let gridSize = 40
-
-for (let i = 0; i < gridSize; i++) {
-  let parentDiv = document.createElement("div")
-  parentDiv.classList.add(`row`)
-
-  for (let j = 0; j < gridSize; j++) {
-    let div = document.createElement("div")
-
-    div.classList.add("block")
-    div.setAttribute("id", `row:${i+1}-col:${j+1}`)
-    
-    div.addEventListener("mouseenter",  () => {
-      div.style.backgroundColor = "#fff"
+function clearGrid(){
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach(block => {
+        block.style.backgroundColor = "";
     })
-
-    parentDiv.appendChild(div);
-  };
-  grid.appendChild(parentDiv)
 }
 
+// GRID
+function createGrid(size){
+    grid.innerHTML = "";
+    for (let i = 0; i < size; i++) {
+        let parentDiv = document.createElement("div")
+        parentDiv.classList.add(`row`)
 
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement("div")
 
-const btn = document.querySelector("#btn-clear");
-btn.addEventListener("click", () => {
-  const blocks = document.querySelectorAll(".block")
- blocks.forEach(block => {
-    block.style.backgroundColor = ""
-  })
+            div.classList.add("block")
+            div.setAttribute("id", `row:${i+1}-col:${j+1}`)
+
+            div.addEventListener("mouseenter",  () => {
+                div.style.backgroundColor = "#fff"
+            })
+
+            parentDiv.appendChild(div);
+        };
+        grid.appendChild(parentDiv)
+    }
+}
+
+createGrid(gridSize);
+
+// Size Buttons
+const sizeBtns = document.querySelectorAll(".box");
+
+sizeBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        clearGrid();
+        let size = Number(btn.id);
+        createGrid(size);
+    });
 })
+
+// button logic
+const btn = document.querySelector("#btn-clear");
+btn.addEventListener("click", clearGrid)
+
+
+
